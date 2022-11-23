ee2cc3d1b5c58825feb4dc0383da0768a1e52a2b
farhasad <yashel.kuyan@gmail.com>
2022-11-23 13:09:39 +0300

    Pervoe izmenenie projekta

---
+++ list-1-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (void)
+
+{ 
+	int  dogs;
+	printf("Skolko uvas sobak?\n");
+	scanf("%d", &dogs);
+	printf("Sledovatelno, u vas %d sobak(a/i)!\n", dogs);
+	return 0;
+} 
---
+++ list-1-2-concrete.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Beton soderzhit pesok i tsement.\n");
+	return 0;
+}
+
